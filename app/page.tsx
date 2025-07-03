5e1fb5075a6f7a7598f57cbed8eca30b98ef34e9
ENSUU <eunsu.dk.kim@gmail.com>
2025-07-03 18:05:20 -0400

    Started project & implemented basic user input detection

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,98 @@
-import Image from "next/image";
+"use client";
+
+import Form from "next/form";
+import { useState } from "react";
+import "./globals.css";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [userInput, setUserInput] = useState("");
+  const [quote, setQuote] = useState(
+    "Hello, my name is David.",
+    // "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+  );
+  const quoteSplit = quote.split(" ");
+  const quoteLength = quoteSplit.length;
+  const [quoteCurrIndex, setQuoteCurrIndex] = useState(0);
+  const [incorrect, setIncorrect] = useState(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  function validateUserWord() {
+    console.log(quoteCurrIndex);
+    const userInputSplit = userInput.split(" ");
+    if (userInputSplit[quoteCurrIndex] === quoteSplit[quoteCurrIndex]) {
+      setIncorrect(false);
+      setQuoteCurrIndex((quoteCurrIndex) => quoteCurrIndex + 1);
+    } else {
+      setIncorrect(true);
+    }
+  }
+
+  function renderIncorrectCorrectCurrentWords(
+    userInput: string,
+    quoteSplit: Array<string>,
+  ) {
+    const userInputSplit = userInput.split(" ");
+    console.log(userInputSplit);
+
+    return quoteSplit.map((word, index) => {
+      const isCurrentWord = index === userInputSplit.length - 1;
+      const isTyped = index < userInputSplit.length;
+
+      let wordClass = "";
+      if (isTyped) {
+        wordClass =
+          quoteSplit[index] == userInputSplit[index]
+            ? "border-b-2 border-green-400"
+            : "border-b-2 border-red-400";
+      } else if (isCurrentWord) {
+        wordClass = "border-b-2 border-black";
+      }
+
+      return (
+        <span key={index} className={"mx-0.5 " + wordClass}>
+          {word}
+        </span>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div id="navbar" className="flex justify-center items-center my-4">
+        <h1 className="text-2xl font-bold">Speedily.</h1>
+      </div>
+      <div id="quote" className="flex justify-center mt-12 mx-auto w-5xl">
+        {renderIncorrectCorrectCurrentWords(userInput, quoteSplit)}
+      </div>
+      <div>
+        <Form className="flex justify-center items-center mt-12" action="">
+          <input
+            type="text"
+            value={userInput}
+            className={
+              "border-2 w-5xl " +
+              (incorrect == null
+                ? "border-black"
+                : incorrect
+                  ? "border-red-600"
+                  : "border-green-600")
+            }
+            onChange={(e) => {
+              setUserInput(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key == " " || e.code == "Space") {
+                validateUserWord();
+              }
+            }}
+            disabled={
+              quoteCurrIndex > quoteSplit.length - 1 && !incorrect
+                ? true
+                : false
+            }
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </Form>
+        {userInput}
+      </div>
     </div>
   );
 }
